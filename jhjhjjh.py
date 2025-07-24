388e41b7e9888a3d63c13716a399496f1e0a7a3a
Cris0Smelted <cristhianstavross@gmail.com>
2025-07-23 20:07:30 -0400

    Create jhjhjjh.py
    gfjhfhghggjhgkuhg

---
+++ jhjhjjh.py
@@ -0,0 +1,32 @@
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+def gen_pass(pass_length):
+    elements = "+-/*!&$#?=@<>1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+    password = ""
+
+    for i in range(pass_length):
+        password += random.choice(elements)
+
+    return password
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+@bot.command()
+async def lmaolol(ctx, count_lmaolol = 8):
+    await ctx.send("lmaolol" * count_lmaolol )
+@bot.command()
+async def pswd(ctx, longitud = 8):
+    await ctx.send(f"Tu contraseña es : {gen_pass(longitud)}")
+bot.run("")
